9af034de4872fe9a6dd227660869ad930a19d82e
Mark Slorach <hello@markslorach.com>
2024-05-11 14:38:15 +0100

    button styling

---
+++ app/components/Pagination.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import {
+  ChevronsLeft,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsRight,
+} from "lucide-react";
+
+interface PaginationProps {
+  itemCount: number;
+  pageSize: number;
+  currentPage: number;
+}
+
+const Pagination = ({ itemCount, pageSize, currentPage }: PaginationProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const pageCount = Math.ceil(itemCount / pageSize);
+  if (pageCount <= 1) return null;
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Button
+        size="sm"
+        variant="secondary"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
+        <ChevronsLeft />
+      </Button>
+      <Button
+        size="sm"
+        variant="secondary"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
+        <ChevronLeft />
+      </Button>
+      <Button
+        size="sm"
+        variant="secondary"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
+        <ChevronRight />
+      </Button>
+      <Button
+        size="sm"
+        variant="secondary"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
+        <ChevronsRight />
+      </Button>
+
+      <span>
+        Page {currentPage} of {pageCount}
+      </span>
+    </div>
+  );
+};
+
+export default Pagination;
